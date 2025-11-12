a8af036dfbf646ea5f7b12918a9fc28c06690f9d
Dirk <45898176+dvkcds@users.noreply.github.com>
2025-11-12 18:53:33 +0100

    revert next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
