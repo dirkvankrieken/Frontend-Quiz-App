460ee1dc0d6c4b105990b2ece0a83e9c4b342144
Dirk <45898176+dvkcds@users.noreply.github.com>
2025-11-13 15:08:34 +0100

    change text color and center element

--- src/app/components/NothingSelected/NothingSelected.tsx
+++ src/app/components/NothingSelected/NothingSelected.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function NothingSelected() {
   return (
-    <div className="flex items-center">
+    <div className="flex text-red-500 items-center justify-center">
       <Image src="assets/images/icon-error.svg" width="50" height="50" alt="" />
       Please select an answer
     </div>
