0fa15eafd35469af5bc38643d809518383015226
Dirk <45898176+dvkcds@users.noreply.github.com>
2025-11-15 09:52:23 +0100

    add iconbackgroundcolor and add hover states

--- src/app/components/QuizSelectionButton/QuizSelectionButton.tsx
+++ src/app/components/QuizSelectionButton/QuizSelectionButton.tsx
@@ -3,22 +3,28 @@ import Image from 'next/image';
 interface quizSelectionButtonProps {
   text: string;
   icon: string;
+  iconBackgroundColor: string;
   onClick: () => void;
 }
 
 const quizSelectionButton = ({
   text,
   icon,
+  iconBackgroundColor,
   onClick,
 }: quizSelectionButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="hover:cursor-pointer dark:text-white dark:bg-blue-850 flex items-center bg-white w-full px-5 py-3 rounded-xl gap-5"
+      className="hover:cursor-pointer hover:dark:!bg-blue-950 hover:bg-gray-50 dark:text-white dark:bg-blue-850 lg:text-2xl text-xl flex items-center bg-white w-full px-5 py-5 rounded-xl gap-5"
     >
-      <div className="bg-white rounded-lg p-2">
-        <Image src={icon} width={50} height={50} alt="" />
-      </div>
+      <Image
+        className={`${iconBackgroundColor} rounded-lg p-2`}
+        src={icon}
+        width={50}
+        height={50}
+        alt=""
+      />
       {text}
     </button>
   );
